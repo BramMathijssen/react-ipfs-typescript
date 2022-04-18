442989f715b71386444fa547924db77003e7101e
Bram Mathijssen <Bram.mathijssen@gmail.com>
2022-04-18 19:27:15 +0200

    ipfs react ts init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { create, CID, IPFSHTTPClient } from "ipfs-http-client";
 
 function App() {
+  let ipfs: IPFSHTTPClient | undefined;
+  try {
+    ipfs = create({
+      url: "https://ipfs.infura.io:5001/api/v0",
+    });
+  } catch (error) {
+    console.error("IPFS error ", error);
+    ipfs = undefined;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!ipfs && (
+          <p>Oh oh, Not connected to IPFS. Checkout out the logs for errors</p>
+        )}
+        <p> connected to ipfs</p>
       </header>
     </div>
   );
